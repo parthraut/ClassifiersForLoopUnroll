a97293f251ac7e667d03b1fe8560f44b80c17ebb
Parth Raut <praut@umich.edu>
2023-12-02 13:56:29 -0500

    changed .gitignore to stop ignoring dataset

--- clang_pass/clang_pass.cpp
+++ clang_pass/clang_pass.cpp
@@ -10,62 +10,75 @@ using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
-class LoopTimingInserter : public MatchFinder::MatchCallback {
-public:
-    LoopTimingInserter(Rewriter &Rewrite) : Rewrite(Rewrite) {}
+// class LoopTimingInserter : public MatchFinder::MatchCallback {
+// public:
+//     LoopTimingInserter(Rewriter &Rewrite) : Rewrite(Rewrite) {}
 
-    virtual void run(const MatchFinder::MatchResult &Result) {
-        if (const Stmt *Loop = Result.Nodes.getNodeAs<Stmt>("loop")) {
-            int LoopNestingLevel = getNestingLevel(Result, Loop);
+//     virtual void run(const MatchFinder::MatchResult &Result) {
+//         if (const Stmt *Loop = Result.Nodes.getNodeAs<Stmt>("loop")) {
+//             int LoopNestingLevel = getNestingLevel(Result, Loop);
 
-            // Get the start location of the loop to insert code before it
-            SourceLocation StartLoc = Loop->getBeginLoc();
+//             // Get the start location of the loop to insert code before it
+//             SourceLocation StartLoc = Loop->getBeginLoc();
 
-            // Insert timing start code and pragma directive before the loop
-            std::string TimingStartCode = "auto start = std::chrono::high_resolution_clock::now();\n";
-            std::string PragmaDirective = "#pragma clang loop unroll_count(" + std::to_string(LoopNestingLevel == 0 ? N : 1) + ")\n";
-            Rewrite.InsertTextBefore(StartLoc, TimingStartCode + PragmaDirective);
+//             // Insert timing start code and pragma directive before the loop
+//             std::string TimingStartCode = "auto start = std::chrono::high_resolution_clock::now();\n";
+//             std::string PragmaDirective = "#pragma clang loop unroll_count(" + std::to_string(LoopNestingLevel == 0 ? N : 1) + ")\n";
+//             Rewrite.InsertTextBefore(StartLoc, TimingStartCode + PragmaDirective);
 
-            // Get the end location of the loop to insert code after it
-            // This assumes that Loop is a compound statement (i.e., enclosed in braces {})
-            if (const auto *CS = dyn_cast<CompoundStmt>(Loop)) {
-                SourceLocation EndLoc = CS->getEndLoc().getLocWithOffset(1);
+//             // Get the end location of the loop to insert code after it
+//             // This assumes that Loop is a compound statement (i.e., enclosed in braces {})
+//             if (const auto *CS = dyn_cast<CompoundStmt>(Loop)) {
+//                 SourceLocation EndLoc = CS->getEndLoc().getLocWithOffset(1);
 
-                // Insert timing end code after the loop
-                std::string TimingEndCode = "\nauto end = std::chrono::high_resolution_clock::now();\n"
-                                            "auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();\n"
-                                            "add_to_loop(duration);\n";
-                Rewrite.InsertTextAfterToken(EndLoc, TimingEndCode);
-            }
-        }
-    }
+//                 // Insert timing end code after the loop
+//                 std::string TimingEndCode = "\nauto end = std::chrono::high_resolution_clock::now();\n"
+//                                             "auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();\n"
+//                                             "add_to_loop(duration);\n";
+//                 Rewrite.InsertTextAfterToken(EndLoc, TimingEndCode);
+//             }
+//         }
+//     }
 
-private:
-    Rewriter &Rewrite;
-};
+// private:
+//     Rewriter &Rewrite;
+// };
 
 int main(int argc, const char **argv) {
-    CommonOptionsParser op(argc, argv);
-    ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+    // Parse command-line options
+    llvm::cl::OptionCategory MyToolCategory("my-tool options");
+    auto ExpectedParser = clang::tooling::CommonOptionsParser::create(argc, argv, MyToolCategory);
+    if (!ExpectedParser) {
+        llvm::errs() << ExpectedParser.takeError();
+        return 1;
+    }
 
-    Rewriter Rewrite;
-    LoopTimingInserter Inserter(Rewrite);
+    // Get the compiler arguments from the parser
+    CommonOptionsParser &op = ExpectedParser.get();
+    // Create a ClangTool instance to run a FrontendAction over a set of files
+    clang::tooling::ClangTool Tool(op.getCompilations(),
+                               op.getSourcePathList());
 
-    MatchFinder Finder;
-    // Define matchers for loops and add to Finder
-    auto ForLoopMatcher = forStmt().bind("forLoop");
-    auto WhileLoopMatcher = whileStmt().bind("whileLoop");
-    auto DoWhileLoopMatcher = doStmt().bind("doWhileLoop");
-    // Use LoopTimingInserter as callback
-    Finder.addMatcher(ForLoopMatcher, &Inserter);
-    Finder.addMatcher(WhileLoopMatcher, &Inserter);
-    Finder.addMatcher(DoWhileLoopMatcher, &Inserter);
 
-    Tool.run(newFrontendActionFactory(&Finder).get());
+    // ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
-    // Output the rewritten source code
-    for (const auto &File : Rewrite.getSourceMgr().fileinfo_begin()) {
-    const auto &ID = File.first;
-    Rewrite.getEditBuffer(ID).write(llvm::outs());
-}
+    // Rewriter Rewrite;
+    // LoopTimingInserter Inserter(Rewrite);
+
+    // MatchFinder Finder;
+    // // Define matchers for loops and add to Finder
+    // auto ForLoopMatcher = forStmt().bind("forLoop");
+    // auto WhileLoopMatcher = whileStmt().bind("whileLoop");
+    // auto DoWhileLoopMatcher = doStmt().bind("doWhileLoop");
+    // // Use LoopTimingInserter as callback
+    // Finder.addMatcher(ForLoopMatcher, &Inserter);
+    // Finder.addMatcher(WhileLoopMatcher, &Inserter);
+    // Finder.addMatcher(DoWhileLoopMatcher, &Inserter);
+
+    // Tool.run(newFrontendActionFactory(&Finder).get());
+
+    // // Output the rewritten source code
+    // for (const auto &File : Rewrite.getSourceMgr().fileinfo_begin()) {
+    // const auto &ID = File.first;
+    // Rewrite.getEditBuffer(ID).write(llvm::outs());
 }
