dee2b90f20139435cc4013151b8b95834638feb5
Parth Raut <praut@umich.edu>
2023-12-13 11:47:54 -0500

    last pass

--- timed_src.cpp
+++ timed_src.cpp
@@ -6,61 +6,124 @@
     extern void add_to_loop(uint64_t, uint64_t);
     extern void print_times();
     extern void init_loops(int);;
+float arr[100000000];
+float select(unsigned long k, unsigned long n)
+{
+  unsigned long i;
+  unsigned long ir;
+  unsigned long j;
+  unsigned long l;
+  unsigned long mid;
+  float a;
+  float temp;
+  int flag;
+  int flag2;
+  l = 1;
+  ir = n;
+  flag = (flag2 = 0);
+  #pragma clang loop unroll(disable)
+  while (!flag)
+  {
+    if (ir <= (l + 1))
+    {
+      if (ir == (l + 1))
+        if (arr[ir] < arr[l])
+      {
+        temp = arr[l];
+        arr[l] = arr[ir];
+        arr[ir] = temp;
+      }
+      flag = 1;
+    }
+    else
+      if (!flag)
+    {
+      mid = (l + ir) >> 1;
+      temp = arr[mid];
+      arr[mid] = arr[l + 1];
+      arr[l + 1] = temp;
+      if (arr[l + 1] > arr[ir])
+      {
+        temp = arr[l + 1];
+        arr[l + 1] = arr[ir];
+        arr[ir] = temp;
+      }
+      if (arr[l] > arr[ir])
+      {
+        temp = arr[l];
+        arr[l] = arr[ir];
+        arr[ir] = temp;
+      }
+      if (arr[l + 1] > arr[l])
+      {
+        temp = arr[l + 1];
+        arr[l + 1] = arr[l];
+        arr[l] = temp;
+      }
+      i = l + 1;
+      j = ir;
+      a = arr[l];
+      #pragma clang loop unroll(disable)
+      while (!flag2)
+      {
+        i++;
+        auto start_0 = std::chrono::high_resolution_clock::now();
+            #pragma clang loop unroll_count(1)
+        while (arr[i] < a)
+          i++;
+
+        auto end_0 = std::chrono::high_resolution_clock::now();
+            auto duration_0 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_0 - start_0);
+            add_to_loop(0, duration_0.count());
+        j--;
+        auto start_1 = std::chrono::high_resolution_clock::now();
+            #pragma clang loop unroll_count(1)
+        while (arr[j] > a)
+          j--;
+
+        auto end_1 = std::chrono::high_resolution_clock::now();
+            auto duration_1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_1 - start_1);
+            add_to_loop(1, duration_1.count());
+        if (j < i)
+          flag2 = 1;
+        if (!flag2)
+          temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        ;
+      }
+
+      arr[l] = arr[j];
+      arr[j] = a;
+      if (j >= k)
+        ir = j - 1;
+      if (j <= k)
+        l = i;
+    }
+  }
+
+  return arr[k];
+}
+
+float getRandomFloat()
+{
+  return ((float) rand()) * 0.00005;
+}
+
 int main()
 {
   atexit(print_times);
-  init_loops(4);
-  int v[1000];
-  #pragma clang loop unroll(disable)
-  for (int i = 1; i < 100; i++)
+  init_loops(3);
+  auto start_2 = std::chrono::high_resolution_clock::now();
+            #pragma clang loop unroll_count(1)
+  for (int i = 0; i < 100000000; i++)
   {
-    #pragma clang loop unroll(disable)
-    for (int j = 0; j < 1000; j++)
-    {
-      auto start_0 = std::chrono::high_resolution_clock::now();
-            #pragma clang loop unroll_count(8)
-      for (int k = 0; k < 1000; k++)
-      {
-        v[i - 1] *= (2 + j) + k;
-      }
-
-      auto end_0 = std::chrono::high_resolution_clock::now();
-            auto duration_0 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_0 - start_0);
-            add_to_loop(0, duration_0.count());
-      auto start_1 = std::chrono::high_resolution_clock::now();
-            #pragma clang loop unroll_count(8)
-      for (int z = 0; z < 345; ++z)
-      {
-        v[i - 1]++;
-      }
-
-      auto end_1 = std::chrono::high_resolution_clock::now();
-            auto duration_1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_1 - start_1);
-            add_to_loop(1, duration_1.count());
-    }
-
-    auto start_2 = std::chrono::high_resolution_clock::now();
-            #pragma clang loop unroll_count(8)
-    for (int mehar = 0; mehar < 380; ++mehar)
-    {
-      v[i - 1] += mehar;
-    }
-
-    auto end_2 = std::chrono::high_resolution_clock::now();
-            auto duration_2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_2 - start_2);
-            add_to_loop(2, duration_2.count());
-    auto start_3 = std::chrono::high_resolution_clock::now();
-            #pragma clang loop unroll_count(8)
-    for (int mehar = 1; mehar < 500; ++mehar)
-    {
-      v[mehar] = v[mehar - 1] + 100000000;
-    }
-
-    auto end_3 = std::chrono::high_resolution_clock::now();
-            auto duration_3 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_3 - start_3);
-            add_to_loop(3, duration_3.count());
+    arr[i] = getRandomFloat();
   }
 
-  return 0;
+  auto end_2 = std::chrono::high_resolution_clock::now();
+            auto duration_2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_2 - start_2);
+            add_to_loop(2, duration_2.count());
+  select(100000000 / 2, 100000000);
 }
 
